a2dc90da95bcf6837a93766e1109c2f36ad11bb4
amartinl89 <andermartin88@gmail.com>
2024-03-08 03:48:17 +0100

    Añadidos elementos visuales y añadidos strings para poner

---
+++ app/src/main/java/com/example/cinemapp/AnadirPelicula.java
@@ -0,0 +1,13 @@
+package com.example.cinemapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AnadirPelicula extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.anadir_pelicula);
+    }
+}
